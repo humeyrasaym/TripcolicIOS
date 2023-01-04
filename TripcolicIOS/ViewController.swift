4b54339059d8e5806428aff49903788f8719d103
humeyrasaym <humeyrasaym@gmail.com>
2023-01-04 14:03:46 +0300

    Initial Commit

---
+++ TripcolicIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripcolicIOS
+//
+//  Created by Hümeyra SAYIM on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
